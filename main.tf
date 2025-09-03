454a812908f8385db8a7d90e8e026ee8b32858c9
Shubhamrathhi <shubhamrathi2210@gmail.com>
2025-09-03 20:39:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,13 @@ provider "aws" {
 # VPC + Networking
 # -----------------
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MainVPC"
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -14,16 +20,28 @@ resource "aws_subnet" "public" {
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1a"
+
+  tags = {
+    Name = "PublicSubnet"
+  }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-southeast-1a"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "InternetGateway"
+  }
 }
 
 resource "aws_route_table" "public" {
@@ -32,6 +50,10 @@ resource "aws_route_table" "public" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "PublicRT"
   }
 }
 
@@ -43,11 +65,21 @@ resource "aws_route_table_association" "public" {
 # NAT Gateway
 resource "aws_eip" "nat" {
   domain = "vpc"
+
+  tags = {
+    Name = "NatEIP"
+  }
 }
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
+
+  depends_on = [aws_internet_gateway.gw]
+
+  tags = {
+    Name = "NatGateway"
+  }
 }
 
 resource "aws_route_table" "private" {
@@ -56,6 +88,10 @@ resource "aws_route_table" "private" {
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "PrivateRT"
   }
 }
 
@@ -69,12 +105,13 @@ resource "aws_route_table_association" "private" {
 # -----------------
 resource "aws_security_group" "bastion_sg" {
   vpc_id = aws_vpc.main.id
+  name   = "BastionSG"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+    cidr_blocks = ["0.0.0.0/0"] # Restrict in real use
   }
 
   egress {
@@ -83,48 +120,30 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_security_group" "jenkins_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Jenkins Web UI
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "BastionSG"
   }
 }
 
-resource "aws_security_group" "sonarqube_sg" {
+resource "aws_security_group" "app_sg" {
   vpc_id = aws_vpc.main.id
+  name   = "AppPrivateSG"
 
+  # Allow Jenkins/SonarQube Web UI (8080) from Bastion only
   ingress {
-    from_port       = 9000
-    to_port         = 9000
+    from_port       = 8080
+    to_port         = 8080
     protocol        = "tcp"
-    security_groups = [aws_security_group.jenkins_sg.id] # only Jenkins can access SonarQube
+    security_groups = [aws_security_group.bastion_sg.id]
   }
 
+  # Allow SSH from Bastion
   ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id] # SSH via Bastion
+    security_groups = [aws_security_group.bastion_sg.id]
   }
 
   egress {
@@ -132,6 +151,10 @@ resource "aws_security_group" "sonarqube_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "AppPrivateSG"
   }
 }
 
@@ -139,52 +162,40 @@ resource "aws_security_group" "sonarqube_sg" {
 # EC2 Instances
 # -----------------
 resource "aws_instance" "bastion" {
-  ami           = "ami-0f62d9254ca98e1aa" # Ubuntu 22.04 (example)
+  ami           = "ami-0df7a207adb9748c7" # Ubuntu 22.04
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
-  key_name      = "my-key"
+  key_name      = "222"
 
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+
   tags = {
     Name = "BastionHost"
   }
 }
 
-resource "aws_instance" "jenkins" {
-  ami           = "ami-0f62d9254ca98e1aa"
-  instance_type = "t2.medium"
-  subnet_id     = aws_subnet.public.id
-  key_name      = "my-key"
-
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  tags = {
-    Name = "JenkinsServer"
-  }
-}
-
-resource "aws_instance" "sonarqube" {
-  ami           = "ami-0f62d9254ca98e1aa"
-  instance_type = "t2.medium"
+resource "aws_instance" "app" {
+  ami           = "ami-0df7a207adb9748c7"
+  instance_type = "c7i-flex.large"
   subnet_id     = aws_subnet.private.id
-  key_name      = "my-key"
+  key_name      = "222"
 
-  vpc_security_group_ids = [aws_security_group.sonarqube_sg.id]
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
   tags = {
-    Name = "SonarQubeServer"
+    Name = "PrivateAppServer"
   }
 }
 
 # -----------------
 # Outputs
 # -----------------
-output "bastion_ip" {
-  value = aws_instance.bastion.public_ip
+output "bastion_public_ip" {
+  description = "Public IP of Bastion Host"
+  value       = aws_instance.bastion.public_ip
 }
 
-output "jenkins_ip" {
-  value = aws_instance.jenkins.public_ip
-}
-
-output "sonarqube_private_ip" {
-  value = aws_instance.sonarqube.private_ip
+output "app_private_ip" {
+  description = "Private IP of App (Jenkins/SonarQube) Server"
+  value       = aws_instance.app.private_ip
 }
